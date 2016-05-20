cdb3a9c9dd2f29d3c406135e6601bcf39a00c1e0
zhengxie <xiez1989@gmail.com>
2016-05-20 15:14:06 +0800

    Add role permissions

--- seahub/settings.py
+++ seahub/settings.py
@@ -501,6 +501,46 @@ ENABLE_GLOBAL_ADDRESSBOOK = True
 #####################
 ENABLE_FOLDER_PERM = False
 
+####################
+# Role permissions #
+####################
+# default permissions:
+# 'default': {
+#     'can_add_repo': True,
+#     'can_add_group': True,
+#     'can_view_org': True,
+#     'can_user_global_address_book': True,
+#     'can_generate_shared_link': True,
+#     'can_drag_drop_folder_to_sync': True,
+#     'can_connect_with_android_clients': True,
+#     'can_connect_with_ios_clients': True,
+#     'can_connect_with_desktop_clients': True,
+#     'can_invite_guest': False,
+#     'can_export_files_via_mobile_client': True,
+# }
+from seahub.constants import GUEST_USER
+ENABLED_ROLE_PERMISSIONS = {
+    GUEST_USER: {
+        'can_add_repo': False,
+        'can_add_group': False,
+        'can_view_org': False,
+        'can_use_global_address_book': False,
+        'can_generate_shared_link': False,
+        'can_drag_drop_folder_to_sync': False,
+        'can_connect_with_android_clients': False,
+        'can_connect_with_ios_clients': False,
+        'can_connect_with_desktop_clients': False,
+        'can_export_files_via_mobile_client': False,
+        # leave other permissions as same as 'default'
+    },
+    'employee': {
+        'can_invite_guest': True,
+    },
+    'test': {
+        'can_add_repo': False,
+    }
+}
+
 #####################
 # Sudo Mode #
 #####################
